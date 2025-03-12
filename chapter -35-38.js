c59d9110abea3ea70f3c07a6988f02397154f43f
inshah20 <inshahjamal26@gmail.com>
2025-03-12 05:34:22 +0500

    made changes

--- chapter -35-38.js
+++ chapter -35-38.js
@@ -241,3 +241,4 @@
 // let area = calcArea(radius);
 // let circumference = calcCircumference(radius);
 // document.write("The circumference of a circle is: " + circumference + "<br />" + "The area of a circle is: " + area)
+//-------------------------- X ------------------------------- X -------------------------------- X -------------------------
